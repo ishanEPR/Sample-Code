53acaaeeccbe345e25a4b50b90619114453c1a7b
ishan <reshmikaediriweera1997@gmail.com>
2021-01-11 11:50:36 +0530

    Add Ratchet library files

---
+++ ChatApplication/src/Chat.php
@@ -0,0 +1,32 @@
+<?php
+namespace MyApp;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+
+    private $clients;
+
+    public function __construct(){
+        $this->clients=array();
+    }
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients[]=$conn;
+        echo "New Connection";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach($this->clients as $client){
+            if($client!=$from){
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        echo "Connection Close";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+}
